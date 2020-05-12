2b3dbbd384576526c5b830d46cada752f004a235
Nina Weber <n.weber@marathon-berlin.de>
2020-05-12 22:51:03 +0200

    Remove AddTask Form from below Timeline

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="timeline">
     <Calendar class="mt-10" />
-    <TaskForm />
   </div>
 </template>
 
 <script>
 import Calendar from "@/components/timeline/Calendar";
-import TaskForm from "@/components/task-form/TaskForm";
 
 export default {
   name: "Timeline",
   components: {
-    Calendar,
-    TaskForm
+    Calendar
   }
 };
 </script>

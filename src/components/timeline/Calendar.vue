2141ce2297689c8e3bdd65f39178fe9de7d634f9
Nina Weber <n.weber@marathon-berlin.de>
2020-05-17 18:36:15 +0200

    Load more days when reaching start/end of timeline

--- src/components/timeline/Calendar.vue
+++ src/components/timeline/Calendar.vue
@@ -58,8 +58,30 @@ export default {
       const offsetRight = this.offsetX + e.target.clientWidth;
       const calendarWidth = this.days.length * this.columnWidth;
       if (calendarWidth <= offsetRight) {
-        console.log("load more days");
+        this.loadFutureDays();
+      } else if (this.offsetX <= 0) {
+        this.loadPastDays();
       }
+    },
+    loadFutureDays() {
+      const lastDay = this.days[this.days.length - 1];
+      const range = moment
+        .range(moment(lastDay).add(1, "d"), moment(lastDay).add(6, "w"))
+        .by("day");
+      const newDays = Array.from(range).map(d => d.startOf("d"));
+      this.days = [...this.days, ...newDays];
+    },
+    loadPastDays() {
+      const firstDay = this.days[0];
+      const range = moment
+        .range(
+          moment(firstDay).subtract(6, "w"),
+          moment(firstDay).subtract(1, "d")
+        )
+        .by("day");
+      const newDays = Array.from(range).map(d => d.startOf("d"));
+      this.days = [...newDays, ...this.days];
+      this.$el.scrollTo(newDays.length * this.columnWidth, 0);
     }
   },
   mounted() {
@@ -71,7 +93,7 @@ export default {
   data() {
     const range = moment
       .range(moment().subtract(3, "weeks"), moment().add(6, "weeks"))
-      .by("day", { excludeEnd: true });
+      .by("day");
     return {
       days: Array.from(range).map(d => d.startOf("d")),
       columnWidth: 50,

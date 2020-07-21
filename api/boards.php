ef16dc4b1a801f9314347eea3b10f76d5ae56cc0
canberkcelik <cnbrk0643@gmail.com>
2020-07-21 14:02:21 +0200

    backend Update
    -„filename“ wird angepasst
    -If-Schleife für CRUD Operationen
    -„delete: UNLINK „ syntax wird aufgepasst
    -Prüft, ob File existiert
    -JSON_Encode für Contents
    -Comments wird gelöscht
    -Server schickt auch Response zurück
    -HTTP Response Code hinzugefügt

--- api/boards.php
+++ api/boards.php
@@ -1,24 +1,25 @@
 <?php
-
 $method = $_SERVER['REQUEST_METHOD'];
 $params = $_GET;
-$payload = json_decode(file_get_contents('php://input'));
-$filename = '../data/boards/${params["id"]}.json';
+$payload = file_get_contents('php://input');
+$filename = '../data/' . $params["id"] . '.json';
 
-var_dump([
-  'method' => $method,
-  'params' => $params,
-  'payload' => $payload,
-  'filename' => $filename,
-]);
-
-// Try in browser console of client app and look at response in network tab
-// fetch('api/boards.php?id=3')
-// fetch('api/boards.php?id=3', { method: 'post', body: JSON.stringify({foo: 'bar'}) })
-
-// Then you can do something like this
-// for post and put: file_put_contents($filename, $payload)
-// for get: file_get_contents($filename)
-// for delete: unlink($filename)
-
+if($method === "POST") {
+  file_put_contents($filename, $payload);
+}else if($method === "DELETE") {
+  unlink($filename);
+}else if($method === "GET") {
+  if (file_exists($filename))  
+  {  
+    $result = file_get_contents($filename, false);
+    header('Content-Type: application/json');
+    echo json_encode($result);
+  } 
+  else 
+  { 
+    echo http_response_code(405);
+  }  
+}else {
+    echo http_response_code(404);
+}
 ?>
